e6695a2acd20cf3c4f8970a7f315ea4dba40d687
asifahmed1810 <asifahmed18oct@gmail.com>
2025-07-16 23:58:49 +0600

    Conntected to the mongoDB using mongoose

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express ,{Application, Request, Response} from 'express'
+
+
+const app:Application=express();
+
+app.get('/',(req:Request , res:Response)=>{
+    res.send('Welcome To Library Management app')
+} )
+
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import {Server} from 'http'
+import mongoose from "mongoose";
+import app from './app';
+
+let server:Server;
+
+const PORT=5000;
+
+async function main() {
+    try {
+        await mongoose.connect('mongodb+srv://library:yZ5ehBPYL4Lo0FHT@cluster1.bhwpr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1')
+        console.log("Connected to MongoDB using Mongoose");
+        server=app.listen(`Application is running on PORT-${PORT}`)
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+main();
