e426c6ac49693da0b8c59bdd4b09e6d137adfc01
asifahmed1810 <asifahmed18oct@gmail.com>
2025-07-17 00:32:18 +0600

    Update tsconfig file

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,22 @@
-import {Server} from 'http'
-import mongoose from "mongoose";
+import { Server } from 'http';
+import mongoose from 'mongoose';
 import app from './app';
 
-let server:Server;
+let server: Server;
 
-const PORT=5000;
+const PORT = 5000;
 
 async function main() {
     try {
-        await mongoose.connect('mongodb+srv://library:yZ5ehBPYL4Lo0FHT@cluster1.bhwpr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1')
+        await mongoose.connect('mongodb+srv://library:yZ5ehBPYL4Lo0FHT@cluster1.bhwpr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1');
         console.log("Connected to MongoDB using Mongoose");
-        server=app.listen(`Application is running on PORT-${PORT}`)
+
+        server = app.listen(PORT, () => {
+            console.log(`Application is running on PORT- ${PORT}`);
+        });
 
     } catch (error) {
-        console.log(error);
+        console.log("The error is", error);
     }
 }
 main();
