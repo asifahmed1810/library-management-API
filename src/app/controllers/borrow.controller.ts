0c1a1f3f0f99714842a7f1a8ba2fb6a1e635d67d
asifahmed1810 <asifahmed18oct@gmail.com>
2025-08-21 11:55:08 +0600

    Added borrow controller file

---
+++ src/app/controllers/borrow.controller.ts
@@ -0,0 +1,72 @@
+import express, { Request, Response } from "express";
+import { Borrow } from "../models/borrow.model";
+
+export const borrowRoutes=express.Router();
+
+borrowRoutes.post('create-borrow', async(req:Request , res:Response)=>{
+    try {
+        const {book,quantity,dueDate}=req.body;
+
+        const borrow=await (Borrow as any).borrowBook(book,quantity,dueDate)
+
+        res.status(201).json({
+            success:true,
+            message:"Book borrowed successfully",
+            data:borrow
+        })
+        
+    } catch (error:any) {
+        res.status(400).json({
+            success: false,
+            message:'Borrow request failed',
+            error:error.message
+        })
+        
+    }
+
+})
+
+borrowRoutes.get('/borrow',async(req:Request , res:Response)=>{
+    try {
+        const summary=await Borrow.aggregate([
+            {
+                $group:{
+                    _id:"$book",
+                    totalQuantity:{$sum:"$quantity"},
+                }
+            },
+            {
+                $lookup:{
+                    from:"books",
+                    localField:"_id",
+                    foreignField:"_id",
+                    as:"book",
+                }
+            },
+            {$unwind:"$book"},
+            {
+                $project:{
+                    _id:0,
+                    book:{
+                        title:"$book.title",
+                        isbn:"$book.isbn",
+                    },
+                    totalQuantity:1,
+                }
+            }
+        ]);
+        res.status(201).json({
+            success:true,
+            message:"Borrowed books summary retrieved successfully",
+            data:summary
+        })
+    } catch (error:any) {
+        res.status(500).json({
+            success:false,
+            message:"Failed to get borrowed books summary",
+            error: error.message
+        })
+        
+    }
+
+})
