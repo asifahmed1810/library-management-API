9ae9ca096d715239a6848546ed8b7018a0983780
asifahmed1810 <asifahmed18oct@gmail.com>
2025-08-21 12:53:01 +0600

    Update borrow controller file and app.ts file

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express ,{Application, Request, Response} from 'express'
 import { bookRoutes } from './app/controllers/book.controller';
+import { borrowRoutes } from './app/controllers/borrow.controller';
 
 
 const app:Application=express();
@@ -7,6 +8,7 @@ const app:Application=express();
 app.use(express.json())
 
 app.use('/api',bookRoutes)
+app.use('/api/borrow',borrowRoutes)
 
 app.get('/',(req:Request , res:Response)=>{
     res.send('Welcome To Library Management app')
